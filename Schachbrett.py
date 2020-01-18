7dcc9ad9f65f96d78b46f885b032b6622b061546
allelein <allelein@alleleins-iMac.fritz.box>
2020-01-18 15:51:17 +0100

    schachproblem

--- Schachbrett.py
+++ Schachbrett.py
@@ -3,4 +3,5 @@ summe = 0
 for feld in range(64):
     reiskorn = 2**feld 
     summe = summe + reiskorn
-    print("Feld {} = {} Reiskörner und damit gesamt {} Reiskörner".format(feld+1,reiskorn,summe))
+    print("Feld {}. = {:,} Reiskörner und damit gesamt {:,} Reiskörner" \
+        .format(feld+1,reiskorn,summe))
