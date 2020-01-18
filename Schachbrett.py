1cef723c2cb753bbd135fe3eab4073224249d372
allelein <allelein@alleleins-iMac.fritz.box>
2020-01-18 23:24:44 +0100

    Schachbrett it Grafik

--- Schachbrett.py
+++ Schachbrett.py
@@ -1,11 +1,16 @@
+import matplotlib.pyplot as plt
 summe = 0
+feldliste = []
 
 for feld in range(64):
     reiskorn = 2**feld 
-    summe = summe + reiskorn
-    print("Feld {}. = {:>30,} Reiskörner und damit gesamt {:>30,} Reiskörner" \
-        .format(feld+1,reiskorn,summe))
+    feldliste.append(reiskorn)
+    summe += reiskorn
+    print(f"Feld {feld+1}. = {reiskorn:>30,} Reiskörner und damit gesamt {summe:>30,} Reiskörner")
 gewicht = summe * 0.02 / 1000 / 1000
 
 print()
-print("Bei einem Gewicht von 0,02g je Korn wiegt das Brett {:,.0f} Tonnen".format(gewicht))
+print(f"Bei einem Gewicht von 0,02g je Korn wiegt das Brett {gewicht:,.0f} Tonnen")
+
+plt.plot(feldliste)
+plt.show()
