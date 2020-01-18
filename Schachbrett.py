47595984f05cace4cf16d47b2639bab85b6a4ed9
allelein <allelein@alleleins-iMac.fritz.box>
2020-01-18 21:19:54 +0100

    dd

--- Schachbrett.py
+++ Schachbrett.py
@@ -6,5 +6,6 @@ for feld in range(64):
     print("Feld {}. = {:>30,} Reiskörner und damit gesamt {:>30,} Reiskörner" \
         .format(feld+1,reiskorn,summe))
 gewicht = summe * 0.02 / 1000 / 1000
+
 print()
 print("Bei einem Gewicht von 0,02g je Korn wiegt das Brett {:,.0f} Tonnen".format(gewicht))
